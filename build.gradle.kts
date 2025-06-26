efec72f377daf99ba59ab613703a827e6360c4c0
Omar Ibrahim <129808006+agiza65@users.noreply.github.com>
2025-06-26 17:19:07 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,50 @@
-// android/build.gradle.kts
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")        // ← 加在这里
+    id("dev.flutter.flutter-gradle-plugin")
+}
 
-import org.gradle.api.tasks.Delete
+android {
+    namespace = "com.example.flutter_application_1"
+    compileSdk = flutter.compileSdkVersion  // 通常是 33
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        // 开启核心库反糖
+        isCoreLibraryDesugaringEnabled = true
     }
-    dependencies {
-        // Android Gradle 插件
-        classpath("com.android.tools.build:gradle:7.4.1")
-        // Google Services 插件
-        classpath("com.google.gms:google-services:4.4.0")
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    defaultConfig {
+        applicationId = "com.example.flutter_application_1"
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+         manifestPlaceholders["GOOGLE_MAPS_API_KEY"]= "AIzaSyCJHu-D1S_dPa7Wzr5Eqw9lZZh5zVt0EuM"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+flutter {
+    source = "../.."
 }
 
-// 将 build 输出移到项目根目录外
-val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.set(newBuildDir)
-
-subprojects {
-    // 各子项目也使用外部 build 目录
-    project.layout.buildDirectory.set(newBuildDir.dir(project.name))
-    // 确保在 app 模块评估前先加载设置
-    evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+dependencies {
+    // Firebase BOM 管理各服务版本
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    // 下面是常用的 Firebase 依赖示例：
+    // implementation("com.google.firebase:firebase-auth")
+    // implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-firestore")
+    add("coreLibraryDesugaring", "com.android.tools:desugar_jdk_libs:1.2.2")
 }
